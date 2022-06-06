f33c0fc96e237d405fcfc24e89d56dc64851768b
InGyu Choi <timetraveler930@gmail.com>
2022-06-06 20:50:11 +0900

    upload setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+sudo apt-get install ros-$ROS_DISTRO-tf2-sensor-msgs ros-$ROS_DISTRO-tf2-geometry-msgs ros-$ROS_DISTRO-mavros*
+
+# dependencies in AirSim build need?
+# sudo apt-get install vulkan-tools vulkan-utils
+# sudo apt-get install -y clang-8 clang++-8 libc++-8-dev libc++abi-8-dev
+# sudo apt-get install -y clang-11 libc++-11-dev libc++abi-11-dev
+# sudo apt-get -y install --no-install-recommends \
+#         lsb-release \
+#         rsync \
+#         software-properties-common \
+#         wget \
+#         libvulkan1
+
+
+
